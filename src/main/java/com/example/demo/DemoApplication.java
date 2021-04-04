888890f3ab6957d34348154fc4c7f9441df2916b
awdesh <awdesh.sharma1@gmail.com>
2021-04-04 12:53:55 -0700

    Update command for the database.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
